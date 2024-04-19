951aa9357d64f5ceaa2f813c660600ebfa9cff53
jsesena <156196985+jsesena@users.noreply.github.com>
2024-04-19 15:47:09 +0200

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('jsesena/nodejsapp')
+            repositoryName('exivan/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
